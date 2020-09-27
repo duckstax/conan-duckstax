6fec41315bbc33bf6490ced64822458a917d7329
kotbegemot <k0tb9g9m0t@yandex.ru>
2020-09-27 10:23:54 +0300

    fix

--- recipes/goblin-engineer/build.py
+++
@@ -1,7 +0,0 @@
-from ci_tools import is_pure_c, get_builder_default
-import os
-
-if __name__ == "__main__":
-    recipe_is_pure_c = is_pure_c()
-    builder = get_builder_default(pure_c=recipe_is_pure_c, cwd=os.path.dirname(os.path.abspath(__file__)))
-    builder.run()
