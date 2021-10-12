ba4d9abfe3c268707b14d6a038d2013e6cdca19e
Alexander <k0tb9g9m0t@yandex.ru>
2021-10-12 11:45:14 +0300

    library updates #5
    * up version actor-zeta 1.0.0a5
    * up version goblin-engineer 1.0.0a4

--- recipes/actor-zeta/all/conanfile.py
+++ recipes/actor-zeta/all/conanfile.py
@@ -4,8 +4,8 @@ import os
 
 class ActorZetaConan(ConanFile):
     name = "actor-zeta"
-    version = "1.0.0a4"
-    description = "actor-zeta is an open source C++11 virtual actor model implementation featuring lightweight & fast and more."
+    version = "1.0.0a5"
+    description = "actor-zeta is an open source C++ virtual actor model implementation featuring lightweight & fast and more."
     url = "https://github.com/cyberduckninja/actor-zeta"
     homepage = "https://github.com/cyberduckninja/actor-zeta"
     author = "kotbegemot <aa.borgardt@yandex.ru>"
@@ -18,15 +18,19 @@ class ActorZetaConan(ConanFile):
     _cmake = None
 
     options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
         "exceptions_disable": [True, False],
         "rtti_disable": [True, False],
-        "shared": [True, False]
+        "cxx_standard": [11, 14, 17]
     }
 
     default_options = {
         "exceptions_disable": False,
         "rtti_disable": False,
-        "shared": False
+        "shared": False,
+        "fPIC": False,
+        "cxx_standard": 11
     }
 
     _source_subfolder = "source_subfolder"
@@ -42,6 +46,7 @@ class ActorZetaConan(ConanFile):
             self._cmake.definitions["EXCEPTIONS_DISABLE"] = self.options.exceptions_disable
             self._cmake.definitions["RTTI_DISABLE"] = self.options.rtti_disable
             self._cmake.definitions["SHARED"] = self.options.shared
+            self._cmake.definitions["CMAKE_CXX_STANDARD"] = self.options.cxx_standard
             self._cmake.configure(build_folder=self._build_subfolder)
         return self._cmake
 
@@ -55,9 +60,11 @@ class ActorZetaConan(ConanFile):
         cmake.build()
 
     def package(self):
+        include_folder = os.path.join(self._source_subfolder, "header/actor-zeta")
         self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
-        self.copy('*.hpp', dst='include/actor-zeta', src='%s/core/actor-zeta' % self._source_subfolder)
-        self.copy('*.ipp', dst='include/actor-zeta', src='%s/core/actor-zeta' % self._source_subfolder)
+        self.copy('*.hpp', dst='include/actor-zeta', src=include_folder)
+        self.copy('*.ipp', dst='include/actor-zeta', src=include_folder)
+        self.copy('*.hpp', dst='include/actor-zeta', src=os.path.join(self._source_subfolder, "header"))
         self.copy(pattern="*.dll", dst="bin", keep_path=False)
         self.copy(pattern="*.lib", dst="lib", keep_path=False)
         self.copy(pattern="*.a", dst="lib", keep_path=False)
