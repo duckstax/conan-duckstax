79ea210424309fa942b12a334d4335fe08ce8f83
kotbegemot <k0tb9g9m0t@gmail.com>
2025-03-18 23:49:36 +0200

    wip

--- recipes/otterbrix/all/conanfile.py
+++ recipes/otterbrix/all/conanfile.py
@@ -4,14 +4,33 @@ from conans import ConanFile, CMake, tools
 import os
 
 
-class OtterbrixRecipe(ConanFile):
+class Otterbrix(ConanFile):
     name = "otterbrix"
+    description = "otterbrix is an open-source framework for developing conventional and analytical applications."
+    url = "https://github.com/duckstax/otterbrix"
+    homepage = "https://github.com/duckstax/otterbrix"
+    author = "kotbegemot <k0tb9g9m0t@gmail.com>"
     license = "MIT"
+    exports = ["LICENSE.md"]
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False]}
-    default_options = {"shared": True}
+    options = {"shared": [True, False]}  # Enable shared/static options
     generators = "cmake", "CMakeToolchain", "CMakeDeps"
     exports_sources = "CMakeLists.txt", "components/*", "core/*", "integration/*", "services/*", "LICENSE", "cmake/*"
+
+    default_options = {
+        "shared": True,
+        "actor-zeta:cxx_standard": 17,
+        "actor-zeta:fPIC": True,
+        "actor-zeta:exceptions_disable": False,
+        "actor-zeta:rtti_disable": False,
+    }
+
+    @property
+    def _minimum_cpp_standard(self):
+        return 17
+
+    def layout(self):
+        cmake_layout(self)
 
     def requirements(self):
         self.requires("boost/1.86.0@")
@@ -39,10 +58,41 @@ class OtterbrixRecipe(ConanFile):
         cmake.build()
 
     def package(self):
-        cmake = CMake(self)
-        cmake.install()
+        self.copy("*.hpp", dst="include/otterbrix", src="integration/cpp")
+        self.copy("*.h", dst="include/otterbrix", src="integration/cpp")
+        self.copy("*.hpp", dst="include", src=".")
+        self.copy("*.h", dst="include", src=".")
+        self.copy("*.dll", dst="bin", keep_path=False)  # Windows shared library
+        self.copy("*.so", dst="lib", keep_path=False)   # Linux shared library
+        self.copy("*.dylib", dst="lib", keep_path=False) # macOS shared library
+        self.copy("*.a", dst="lib", keep_path=False)    # Static library (if needed)
 
     def package_info(self):
-        self.cpp_info.libs = ["otterbrix_document", "otterbrix_types", "otterbrix_cursor",
-                              "otterbrix_session", "otterbrix_expressions", "otterbrix_logical_plan",
-                              "cpp_otterbrix"]
+        self.cpp_info.components["cpp_otterbrix"].libs = ["cpp_otterbrix"]
+        self.cpp_info.components["cpp_otterbrix"].requires.append("otterbrix_document")
+        self.cpp_info.components["cpp_otterbrix"].requires.append("otterbrix_types")
+        self.cpp_info.components["cpp_otterbrix"].requires.append("otterbrix_cursor")
+        self.cpp_info.components["cpp_otterbrix"].requires.append("otterbrix_session")
+        self.cpp_info.components["cpp_otterbrix"].requires.append("otterbrix_expressions")
+        self.cpp_info.components["cpp_otterbrix"].requires.append("otterbrix_logical_plan")
+        self.cpp_info.components["cpp_otterbrix"].requires.append("otterbrix_sql")
+        self.cpp_info.components["cpp_otterbrix"].requires.append("boost::boost")
+        self.cpp_info.components["cpp_otterbrix"].requires.append("abseil::abseil")
+        self.cpp_info.components["cpp_otterbrix"].requires.append("actor-zeta::actor-zeta")
+        self.cpp_info.components["cpp_otterbrix"].requires.append("magic_enum::magic_enum")
+        self.cpp_info.components["cpp_otterbrix"].requires.append("msgpack-cxx::msgpack-cxx")
+        self.cpp_info.components["cpp_otterbrix"].requires.append("fmt::fmt")
+        self.cpp_info.components["cpp_otterbrix"].requires.append("spdlog::spdlog")
+        self.cpp_info.components["cpp_otterbrix"].requires.append("crc32c::crc32c")
+        self.cpp_info.components["cpp_otterbrix"].requires.append("zlib::zlib")
+        self.cpp_info.components["cpp_otterbrix"].requires.append("bzip2::bzip2")
+
+        self.cpp_info.components["otterbrix_document"].libs = ["otterbrix_document"]
+        self.cpp_info.components["otterbrix_types"].libs = ["otterbrix_types"]
+        self.cpp_info.components["otterbrix_cursor"].libs = ["otterbrix_cursor"]
+        self.cpp_info.components["otterbrix_session"].libs = ["otterbrix_session"]
+        self.cpp_info.components["otterbrix_expressions"].libs = ["otterbrix_expressions"]
+        self.cpp_info.components["otterbrix_logical_plan"].libs = ["otterbrix_logical_plan"]
+        self.cpp_info.components["otterbrix_sql"].libs = ["otterbrix_sql"]
+
+        self.cpp_info.includedirs = ["include"]
