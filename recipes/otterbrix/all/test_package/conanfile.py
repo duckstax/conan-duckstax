582a3aa5ae07ecafed31e94a83c994a0ab2113c3
kotbegemot <k0tb9g9m0t@gmail.com>
2025-04-17 23:17:46 +0300

    update otterbrix

--- recipes/otterbrix/all/test_package/conanfile.py
+++ recipes/otterbrix/all/test_package/conanfile.py
@@ -11,12 +11,18 @@ class OtterbrixTestConan(ConanFile):
 
     def requirements(self):
         self.requires(self.tested_reference_str)
-        self.requires("arrow/19.0.1")
-        self.requires("boost/1.86.0@", force=True)
-        self.requires("abseil/20230802.1@")
+        self.requires("boost/1.86.0", force=True)
+        self.requires("fmt/11.1.3")
+        self.requires("spdlog/1.15.1")
+        self.requires("pybind11/2.10.0")
+        self.requires("msgpack-cxx/4.1.1")
+        self.requires("catch2/2.13.7")
+        self.requires("abseil/20230802.1")
+        self.requires("benchmark/1.6.1")
         self.requires("zlib/1.2.12")
         self.requires("bzip2/1.0.8")
-        self.requires("catch2/2.13.7@")
+        self.requires("magic_enum/0.8.1")
+        self.requires("actor-zeta/1.0.0a12")
 
     def layout(self):
         cmake_layout(self)
