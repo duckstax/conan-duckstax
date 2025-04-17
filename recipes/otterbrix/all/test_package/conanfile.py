18074a12469d8fd0ee9722f62331a9ed57f1a101
theirix <theirix@gmail.com>
2025-04-17 20:52:21 +0100

    Fix boost dependency

--- recipes/otterbrix/all/test_package/conanfile.py
+++ recipes/otterbrix/all/test_package/conanfile.py
@@ -12,7 +12,7 @@ class OtterbrixTestConan(ConanFile):
     def requirements(self):
         self.requires(self.tested_reference_str)
         self.requires("arrow/19.0.1")
-        self.requires("boost/1.86.0@", override=True)
+        self.requires("boost/1.86.0@", force=True)
         self.requires("abseil/20230802.1@")
         self.requires("zlib/1.2.12")
         self.requires("bzip2/1.0.8")
