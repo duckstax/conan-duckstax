36793a89293a6b82dc6dc6dc8d5a1fd032260dea
Alexandr <k0tb9g9m0t@gmail.com>
2025-04-18 10:08:37 +0300

    otterbrix recipe update (#22)
    ---------
    
    Co-authored-by: OrangeSmokingJacket <whereisgeol@gmail.com>
    Co-authored-by: theirix <theirix@gmail.com>

--- recipes/otterbrix/all/conanfile.py
+++ recipes/otterbrix/all/conanfile.py
@@ -1,7 +1,9 @@
 from conan import ConanFile
 from conan.tools.cmake import CMake, cmake_layout, CMakeDeps, CMakeToolchain
-from conan.tools.files import copy, get, rmdir, save, load
+from conan.tools.files import copy, get, rmdir, save, load, collect_libs
 import os
+from glob import glob
+
 
 class Otterbrix(ConanFile):
     name = "otterbrix"
@@ -55,45 +57,70 @@ class Otterbrix(ConanFile):
         cmake.build()
 
     def package(self):
-        copy(self, "*.hpp", dst=os.path.join(self.package_folder, "include", "otterbrix"), src=os.path.join(self.source_folder, "integration", "cpp"))
-        copy(self, "*.h", dst=os.path.join(self.package_folder, "include"), src=os.path.join(self.source_folder, "integration", "cpp"))
+        copy(self,"*.hpp", dst=os.path.join(self.package_folder, "include/otterbrix"), src=os.path.join(self.source_folder, "integration/cpp"))
+        copy(self,"*.h", dst=os.path.join(self.package_folder, "include/otterbrix"), src=os.path.join(self.source_folder, "integration/cpp"))
         copy(self, "*.hpp", dst=os.path.join(self.package_folder, "include"), src=self.source_folder)
         copy(self, "*.h", dst=os.path.join(self.package_folder, "include"), src=self.source_folder)
 
-        copy(self, "*.dll", dst="bin", src=self.build_folder, keep_path=False)  # Windows shared library
-        copy(self, "*.so", dst="lib", src=self.build_folder, keep_path=False)  # Linux shared library
-        copy(self, "*.dylib", dst="lib", src=self.build_folder, keep_path=False)  # macOS shared library
-        copy(self, "*.a", dst="lib", src=self.build_folder, keep_path=False)  # Static library (if needed)
+        copy(self, "*.dll", src=self.build_folder, dst=os.path.join(self.package_folder, "bin"), keep_path=False)
+        copy(self, "*.lib", src=self.build_folder, dst=os.path.join(self.package_folder, "lib"), keep_path=False)
+        copy(self, "*.a", src=self.build_folder, dst=os.path.join(self.package_folder, "lib"), keep_path=False)
+        copy(self, "*.so*", src=self.build_folder, dst=os.path.join(self.package_folder, "lib"), keep_path=False)
+        copy(self, "*.dylib", src=self.build_folder, dst=os.path.join(self.package_folder, "lib"), keep_path=False)
+
+        for f in glob(os.path.join(self.package_folder, "lib", "otterbrix.cpython-*.so")):
+            os.remove(f)
 
     def package_info(self):
-        self.cpp_info.components["cpp_otterbrix"].libs = ["cpp_otterbrix"]
-        self.cpp_info.components["cpp_otterbrix"].requires.append("otterbrix_document")
-        self.cpp_info.components["cpp_otterbrix"].requires.append("otterbrix_types")
-        self.cpp_info.components["cpp_otterbrix"].requires.append("otterbrix_cursor")
-        self.cpp_info.components["cpp_otterbrix"].requires.append("otterbrix_session")
-        self.cpp_info.components["cpp_otterbrix"].requires.append("otterbrix_expressions")
-        self.cpp_info.components["cpp_otterbrix"].requires.append("otterbrix_logical_plan")
-        self.cpp_info.components["cpp_otterbrix"].requires.append("otterbrix_sql")
-        self.cpp_info.components["cpp_otterbrix"].requires.append("boost::boost")
-        self.cpp_info.components["cpp_otterbrix"].requires.append("abseil::abseil")
-        self.cpp_info.components["cpp_otterbrix"].requires.append("actor-zeta::actor-zeta")
-        self.cpp_info.components["cpp_otterbrix"].requires.append("magic_enum::magic_enum")
-        self.cpp_info.components["cpp_otterbrix"].requires.append("msgpack-cxx::msgpack-cxx")
-        self.cpp_info.components["cpp_otterbrix"].requires.append("fmt::fmt")
-        self.cpp_info.components["cpp_otterbrix"].requires.append("spdlog::spdlog")
-        self.cpp_info.components["cpp_otterbrix"].requires.append("zlib::zlib")
-        self.cpp_info.components["cpp_otterbrix"].requires.append("bzip2::bzip2")
-        # TODO: recheck usage by the component
-        self.cpp_info.components["cpp_otterbrix"].requires.append("pybind11::pybind11")
-        self.cpp_info.components["cpp_otterbrix"].requires.append("catch2::catch2")
-        self.cpp_info.components["cpp_otterbrix"].requires.append("benchmark::benchmark")
+        self.cpp_info.set_property("cmake_file_name", "otterbrix")
+        self.cpp_info.set_property("cmake_target_name", "otterbrix::otterbrix")
 
-        self.cpp_info.components["otterbrix_document"].libs = ["otterbrix_document"]
-        self.cpp_info.components["otterbrix_types"].libs = ["otterbrix_types"]
-        self.cpp_info.components["otterbrix_cursor"].libs = ["otterbrix_cursor"]
-        self.cpp_info.components["otterbrix_session"].libs = ["otterbrix_session"]
-        self.cpp_info.components["otterbrix_expressions"].libs = ["otterbrix_expressions"]
-        self.cpp_info.components["otterbrix_logical_plan"].libs = ["otterbrix_logical_plan"]
-        self.cpp_info.components["otterbrix_sql"].libs = ["otterbrix_sql"]
-
+        self.cpp_info.libs = collect_libs(self)
         self.cpp_info.includedirs = ["include"]
+        self.cpp_info.libdirs = ["lib"]
+        self.cpp_info.requires = []
+        self.cpp_info.requires.append("boost::boost")
+        self.cpp_info.requires.append("abseil::abseil")
+        self.cpp_info.requires.append("actor-zeta::actor-zeta")
+        self.cpp_info.requires.append("magic_enum::magic_enum")
+        self.cpp_info.requires.append("msgpack-cxx::msgpack-cxx")
+        self.cpp_info.requires.append("fmt::fmt")
+        self.cpp_info.requires.append("spdlog::spdlog")
+        self.cpp_info.requires.append("zlib::zlib")
+        self.cpp_info.requires.append("bzip2::bzip2")
+        # TODO: recheck usage by the component
+        self.cpp_info.requires.append("pybind11::pybind11")
+        self.cpp_info.requires.append("catch2::catch2")
+        self.cpp_info.requires.append("benchmark::benchmark")
+        self.cpp_info.includedirs = ["include"]
+
+        #self.cpp_info.components["otterbrix"].requires.append("otterbrix_document")
+        #self.cpp_info.components["otterbrix"].requires.append("otterbrix_types")
+        #self.cpp_info.components["otterbrix"].requires.append("otterbrix_cursor")
+        #self.cpp_info.components["otterbrix"].requires.append("otterbrix_session")
+        #self.cpp_info.components["otterbrix"].requires.append("otterbrix_expressions")
+        #self.cpp_info.components["otterbrix"].requires.append("otterbrix_sql")
+        #self.cpp_info.components["otterbrix"].requires.append("otterbrix_logical_plan")
+        #self.cpp_info.components["otterbrix"].requires.append("boost::boost")
+        #self.cpp_info.components["otterbrix"].requires.append("abseil::abseil")
+        #self.cpp_info.components["otterbrix"].requires.append("actor-zeta::actor-zeta")
+        #self.cpp_info.components["otterbrix"].requires.append("magic_enum::magic_enum")
+        #self.cpp_info.components["otterbrix"].requires.append("msgpack-cxx::msgpack-cxx")
+        #self.cpp_info.components["otterbrix"].requires.append("fmt::fmt")
+        #self.cpp_info.components["otterbrix"].requires.append("spdlog::spdlog")
+        #self.cpp_info.components["otterbrix"].requires.append("zlib::zlib")
+        #self.cpp_info.components["otterbrix"].requires.append("bzip2::bzip2")
+        # TODO: recheck usage by the component
+        #self.cpp_info.components["otterbrix"].requires.append("pybind11::pybind11")
+        #self.cpp_info.components["otterbrix"].requires.append("catch2::catch2")
+        #self.cpp_info.components["otterbrix"].requires.append("benchmark::benchmark")
+
+        #self.cpp_info.components["otterbrix_document"].libs = ["otterbrix_document"]
+        #self.cpp_info.components["otterbrix_types"].libs = ["otterbrix_types"]
+        #self.cpp_info.components["otterbrix_cursor"].libs = ["otterbrix_cursor"]
+        #self.cpp_info.components["otterbrix_session"].libs = ["otterbrix_session"]
+        #self.cpp_info.components["otterbrix_expressions"].libs = ["otterbrix_expressions"]
+        #self.cpp_info.components["otterbrix_sql"].libs = ["otterbrix_sql"]
+        #self.cpp_info.components["otterbrix_logical_plan"].libs = ["otterbrix_logical_plan"]
+
+        #self.cpp_info.includedirs = ["include"]
---
+++ recipes/otterbrix/all/test_package/conanfile.py
@@ -0,0 +1,45 @@
+from conan import ConanFile
+from conan.tools.cmake import CMake,CMakeToolchain, CMakeDeps, cmake_layout
+import os
+
+
+class OtterbrixTestConan(ConanFile):
+    settings = "os", "arch", "compiler", "build_type"
+    test_type = "explicit"
+
+    exports_sources = "CMakeLists.txt", "src/*", "include/*"
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
+        self.requires("arrow/19.0.1")
+        self.requires("boost/1.86.0", force=True)
+        self.requires("fmt/11.1.3")
+        self.requires("spdlog/1.15.1")
+        self.requires("pybind11/2.10.0")
+        self.requires("msgpack-cxx/4.1.1")
+        self.requires("catch2/2.13.7")
+        self.requires("abseil/20230802.1")
+        self.requires("benchmark/1.6.1")
+        self.requires("zlib/1.2.12")
+        self.requires("bzip2/1.0.8")
+        self.requires("magic_enum/0.8.1")
+        self.requires("actor-zeta/1.0.0a12")
+
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+
+        deps = CMakeDeps(self)
+        deps.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def test(self):
+        if not self.conf.get("tools.build:skip_test", default=False):
+            self.run(os.path.join(self.cpp.build.bindir, "test_package"), env="conanrun")
